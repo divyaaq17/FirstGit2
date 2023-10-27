af6b75b5c7cdb2416aaeabf9264600b8d0fb23ee
divyaaq <divya.aqueleen@gmail.com>
2023-10-27 12:10:25 +0530

    style chnges to last item

--- main.js
+++ main.js
@@ -7,3 +7,9 @@ for(var i=0;i<listitem.length;i++)
 {
     listitem[i].style.fontWeight = "bold"
 }
+
+var newitem = document.getElementsByClassName("anotherclass")
+newitem[0].style.color='blue';
+
+var lastitem = document.getElementsByTagName("li")
+lastitem[4].style.fontWeight='bold';
