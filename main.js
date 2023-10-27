a351c55a766c0f58f43b15bd078e93b7c8ead734
divyaaq <divya.aqueleen@gmail.com>
2023-10-27 12:01:23 +0530

    added style changes

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("Hello")
+//console.log("Hello")
+
+var listitem =  document.getElementsByClassName("list-group-item")
+listitem[2].style.backgroundColor="green";
+
+for(var i=0;i<listitem.length;i++)
+{
+    listitem[i].style.fontWeight = "bold"
+}
