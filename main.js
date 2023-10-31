8b5e41c82b3db5ea37e6b02cc3df3de39f501842
divyaaq <divya.aqueleen@gmail.com>
2023-10-31 12:42:26 +0530

    playing with nodes

--- main.js
+++ main.js
@@ -30,3 +30,57 @@ for(var i=0;i<odditems.length;i++)
 {
     odditems[i].style.backgroundColor = 'green'
 }
+
+//parentelement\
+var itemlist = document.querySelector("#items");
+itemlist.parentElement.style.backgroundColor="#f4f4f4";
+
+//'children' are preferred over 'childnodes'
+//childnode gives nodelist, children gives htmlcollection
+
+itemlist.children[3].style.backgroundColor="yellow";
+
+//lastchildelement
+itemlist.lastElementChild.textContent ="Hello 4";
+
+//last child
+console.log(itemlist.lastChild)
+
+//nextsibling
+console.log(itemlist.nextSibling)
+console.log(itemlist.nextElementSibling)
+
+//previoussibling
+console.log(itemlist.previousSibling)
+console.log(itemlist.previousElementSibling)
+
+
+//create new element
+var newDiv = document.createElement("div")
+//newDiv.className="Hello";
+newDiv.id="hello1";
+newDiv.setAttribute("title","newtitle");
+
+var newtextnode = document.createTextNode('Hello World');
+newDiv.appendChild(newtextnode)
+
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1')
+
+container.insertBefore(newDiv,h1);
+
+//new element before Item1
+
+//create new element
+var newDiv2 = document.createElement("div")
+//newDiv.className="Hello";
+newDiv2.id="hello2";
+newDiv2.setAttribute("title","newtitle2");
+var newtextnode2 = document.createTextNode('Hello World');
+newDiv2.appendChild(newtextnode2)
+
+var items = document.querySelector('div #items')
+var firstItem=  document.querySelector('div li')
+
+items.insertBefore(newDiv2,firstItem)
